107cf4e44d85cbe1cea4af30da0440284802bd76
kye1115z <kye1115z@naver.com>
2022-08-28 05:27:36 +0900

    feat: Starting.js

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Destination from './Destination';
+import Starting from './Starting';
 
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/destination' element={<Destination />} />
+                <Route path='/starting' element={<Starting />} />
 
                 </Routes>
         </BrowserRouter>
